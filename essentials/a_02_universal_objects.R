bd84f0230d390ee3688d4bb45c0484205f87c5da
rmp15 <r.parks15@imperial.ac.uk>
2022-04-11 18:54:09 +0100

    applied gitignore

--- essentials/a_02_universal_objects.R
+++
@@ -1,5 +0,0 @@
-# RStudio Cloud Tutorial for participants in Columbia Mailman SHARP Training
-# Robbie M Parks 2022
-
-# Script to define set of R objects which occur in all scripts
-print('Loading universal objects')
--- essentials/a_03_universal_custom_functions.R
+++
@@ -1,5 +0,0 @@
-# RStudio Cloud Tutorial for participants in Columbia Mailman SHARP Training
-# Robbie M Parks 2022
-
-# Script to define set of functions which are used throughout all scripts
-print('Loading universal custom functions')
